ba37c5a70e1533b07b2a7af3a446df79084fec58
Kevin Arias <kevinarias13m@gmail.com>
2020-07-22 05:07:23 -0500

    The JQueue class was added.

---
+++ queue/JQueue.java
@@ -0,0 +1,20 @@
+package queue;
+
+/**
+ * 
+ * @author Kevin Arias
+ *
+ */
+public class JQueue<T> {
+	private T item;
+	private JQueue<T> head, queue;
+	private int size = 0;
+	
+	public JQueue() {
+		item = null;
+		head = queue = null;
+	}
+	
+	
+
+}
